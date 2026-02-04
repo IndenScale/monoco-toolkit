ed710d8f30eed7192ad8b515e57147f4a8b9cc73
IndenScale <hfu7565ytru@gmail.com>
2026-02-04 11:23:07 +0800

    feat: add test file from FEAT-0173 feature branch

---
+++ test_feat_0173.py
@@ -0,0 +1,6 @@
+# Test file for FEAT-0173
+# This file is created in feature branch to test issue close merge strategy
+
+def test_merge_strategy():
+    """Test that issue close works when both main and feature modify issue file."""
+    return "Feature branch version"
